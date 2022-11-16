ec375b06484ccc11ff69a5d7020cbaeca3e51015
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-11-17 02:27:05 +0900

    feat: create tokenizer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,44 +1,170 @@
-use crate::token::Token;
+use crate::token::{Token, TokenKind, KEYWORDS};
 
-struct Lexer {
-    input: Vec<char>,
+#[derive(Debug)]
+pub struct Lexer<'a> {
+    input: &'a str,
     position: usize,
+    read_position: usize,
+    ch: u8,
 }
 
-impl Lexer {
-    fn new(input: Vec<char>) -> Lexer {
-        Lexer { input, position: 0 }
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str) -> Self {
+        let mut l = Lexer {
+            input,
+            position: 0,
+            read_position: 0,
+            ch: 0,
+        };
+        l.read_char();
+        return l;
     }
 
-    fn next_token(&mut self) -> Option<Token> {
-        if self.position >= self.input.len() {
-            return Some(Token::Eof);
+    pub fn new_token(kind: TokenKind, ch: u8) -> Token {
+        Token {
+            kind,
+            literal: String::from_utf8(vec![ch]).unwrap(),
         }
+    }
 
-        let ch = self.input[self.position];
-        self.position += 1;
+    pub fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
 
-        match ch {
-            '+' => Some(Token::Plus),
-            '-' => Some(Token::Minus),
-            '*' => Some(Token::Asterisk),
-            '/' => Some(Token::Slash),
-            '(' => Some(Token::LParen),
-            ')' => Some(Token::RParen),
-            ' ' => Some(Token::Whitespace),
-            '0'..='9' => Some(Token::Number(ch.to_digit(10).unwrap() as f64)),
-            _ => Some(Token::Illegal),
+        let token;
+        match self.ch {
+            b':' => token = Lexer::new_token(TokenKind::Colon, self.ch),
+            b',' => token = Lexer::new_token(TokenKind::Comma, self.ch),
+            b'(' => token = Lexer::new_token(TokenKind::LeftParen, self.ch),
+            b')' => token = Lexer::new_token(TokenKind::RightParen, self.ch),
+            b'{' => token = Lexer::new_token(TokenKind::LeftBrace, self.ch),
+            b'}' => token = Lexer::new_token(TokenKind::RightBrace, self.ch),
+            b'[' => token = Lexer::new_token(TokenKind::LeftBracket, self.ch),
+            b']' => token = Lexer::new_token(TokenKind::RightBracket, self.ch),
+            b'0'..=b'9' => {
+                token = Token {
+                    kind: TokenKind::Number,
+                    literal: self.read_number(),
+                };
+                return token;
+            }
+            b'a'..=b'z' | b'A'..=b'Z' | b'_' => {
+                let ident = self.read_ident();
+                let kind = KEYWORDS
+                    .get(ident.as_str())
+                    .unwrap_or(&TokenKind::Ident)
+                    .clone();
+                token = Token {
+                    kind,
+                    literal: ident,
+                };
+                return token;
+            }
+            _ => token = Lexer::new_token(TokenKind::Illegal, self.ch),
         }
+        self.read_char();
+        return token;
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch == b' ' || self.ch == b'\n' || self.ch == b'\t' || self.ch == b'\r' {
+            self.read_char();
+        }
+    }
+
+    fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = 0;
+        } else {
+            self.ch = self.input.as_bytes()[self.read_position];
+        }
+        self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    fn read_number(&mut self) -> String {
+        let position = self.position;
+        while self.ch >= b'0' && self.ch <= b'9' {
+            self.read_char();
+        }
+        self.input[position..self.position].to_string()
+    }
+
+    fn is_letter(&self, ch: u8) -> bool {
+        (b'a'..=b'z').contains(&ch) || (b'A'..=b'Z').contains(&ch) || ch == b'_'
+    }
+
+    fn read_ident(&mut self) -> String {
+        let position = self.position;
+        while self.is_letter(self.ch) {
+            self.read_char();
+        }
+        self.input[position..self.position].to_string()
     }
 }
 
 #[test]
-fn test_lexer() {
-    let mut lexer = Lexer::new("1 + 2".chars().collect());
-    assert_eq!(lexer.next_token(), Some(Token::Number(1.0)));
-    assert_eq!(lexer.next_token(), Some(Token::Whitespace));
-    assert_eq!(lexer.next_token(), Some(Token::Plus));
-    assert_eq!(lexer.next_token(), Some(Token::Whitespace));
-    assert_eq!(lexer.next_token(), Some(Token::Number(2.0)));
-    assert_eq!(lexer.next_token(), Some(Token::Eof));
+fn test_next_token() {
+    let input = r#"
+    struct Person {
+        name: string,
+        age: number,
+    }
+    "#;
+    let tests = vec![
+        Token {
+            kind: TokenKind::Struct,
+            literal: "struct".to_string(),
+        },
+        Token {
+            kind: TokenKind::Ident,
+            literal: "Person".to_string(),
+        },
+        Token {
+            kind: TokenKind::LeftBrace,
+            literal: "{".to_string(),
+        },
+        Token {
+            kind: TokenKind::Ident,
+            literal: "name".to_string(),
+        },
+        Token {
+            kind: TokenKind::Colon,
+            literal: ":".to_string(),
+        },
+        Token {
+            kind: TokenKind::String,
+            literal: "string".to_string(),
+        },
+        Token {
+            kind: TokenKind::Comma,
+            literal: ",".to_string(),
+        },
+        Token {
+            kind: TokenKind::Ident,
+            literal: "age".to_string(),
+        },
+        Token {
+            kind: TokenKind::Colon,
+            literal: ":".to_string(),
+        },
+        Token {
+            kind: TokenKind::Number,
+            literal: "number".to_string(),
+        },
+        Token {
+            kind: TokenKind::Comma,
+            literal: ",".to_string(),
+        },
+        Token {
+            kind: TokenKind::RightBrace,
+            literal: "}".to_string(),
+        },
+    ];
+
+    let mut l = Lexer::new(input);
+    for tt in tests {
+        let tok = l.next_token();
+        assert_eq!(tok.kind, tt.kind);
+        assert_eq!(tok.literal, tt.literal);
+    }
 }
--- src/token.rs
+++ src/token.rs
@@ -1,13 +1,45 @@
+use phf::phf_map;
+
 #[derive(Debug, PartialEq, Clone)]
-pub enum Token {
-    Number(f64),
-    Plus,
-    Minus,
-    Asterisk,
-    Slash,
-    LParen,
-    RParen,
-    Whitespace,
-    Illegal,
-    Eof,
+pub enum TokenKind {
+    // Operators
+    And,          // &&
+    Or,           // ||
+    Colon,        // :
+    Comma,        // ,
+    LeftParen,    // (
+    RightParen,   // )
+    LeftBrace,    // {
+    RightBrace,   // }
+    LeftBracket,  // [
+    RightBracket, // ]
+
+    // Keywords
+    Struct,  // struct
+    Enum,    // enum
+    Number,  // number
+    String,  // string
+    Boolean, // boolean
+
+    // Other
+    Ident,
+    Illegal, // Illegal
 }
+
+pub struct Token {
+    pub kind: TokenKind,
+    pub literal: String,
+}
+
+pub static KEYWORDS: phf::Map<&'static str, TokenKind> = phf_map! {
+    "struct" => TokenKind::Struct,
+    "enum" => TokenKind::Enum,
+    "number" => TokenKind::Number,
+    "string" => TokenKind::String,
+    "boolean" => TokenKind::Boolean,
+};
+
+pub static OPERATORS: phf::Map<&'static str, TokenKind> = phf_map! {
+    "&&" => TokenKind::And,
+    "||" => TokenKind::Or,
+};
